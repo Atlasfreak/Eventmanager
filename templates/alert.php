22c1ec5850283f40d2650774e2013486f20e25d8
Per Göttlicher <atlasfreak@gmx.de>
2021-11-26 12:42:33 +0100

    added alert template;

---
+++ templates/alert.php
@@ -0,0 +1,6 @@
+<div class="alert alert-<?=$this->e($type)?> alert-dismissible fade show" role="alert">
+    <?=$this->e($msg)?>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
