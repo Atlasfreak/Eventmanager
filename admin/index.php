3795661f5bfe77eaf88e8e0cb58c1b137a0464d2
Per <atlasfreak@gmx.de>
2021-12-05 16:42:32 +0100

    if no admin is defined you can not login;

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,10 @@ include("../inc/db.php");
 
 include("inc/header.php");
 
+if ($db->query("SELECT id FROM admin")->rowCount() == 0) {
+    die(header("Location: /"));
+}
+
 if(isset($_SESSION["registration_username"],$_SESSION["registration_password"])) {
     include("home.php");
 }
