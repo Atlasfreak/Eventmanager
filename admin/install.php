395bd5190d3d34e5949a30a642cb6b3a12a30073
Per <atlasfreak@gmx.de>
2021-12-05 16:42:00 +0100

    install works as long as not all tables have been created;

--- admin/install.php
+++ admin/install.php
@@ -4,7 +4,7 @@ include("../inc/db.php");
 require("../config.php");
 $query = $db->mysql->prepare("SHOW TABLES;");
 $query->execute();
-if ($query->rowCount() > 0) die(header('Location: index.php'));
+if ($query->rowCount() >= $db->count_tables()) die(header('Location: index.php'));
 
 $db->init_db();
 echo "<meta http-equiv='refresh' content='5; url=../admin/'> Datenbank erfolgreich initialisiert."
