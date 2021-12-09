69bcdf1d45bbf49336aa4a742b9096519404e29d
Per <atlasfreak@gmx.de>
2021-12-09 23:27:06 +0100

    simplified with ??; set defaults for date values to current timestamp;

--- admin/create_event.php
+++ admin/create_event.php
@@ -6,14 +6,15 @@ require "../quill_delta_parser/Lexer.php";
 
 if(!isset($_SESSION["registration_username"],$_SESSION["registration_password"])) {
     header("Location:../admin");
+    exit;
 }
 
-function isJson($string) {
+function isJson(string $string): bool {
     json_decode($string);
     return json_last_error() === JSON_ERROR_NONE;
 }
 
-function render_page($templates, $title_err = false, $description_err = false, $email_template_err = false, $reg_date_err = false, $event_date_err = false, $event_reg_date_err = false, $data = array()) {
+function render_page(\League\Plates\Engine $templates, bool $title_err = false, bool $description_err = false, bool $email_template_err = false, bool $reg_date_err = false, bool $event_date_err = false, bool $event_reg_date_err = false, array $data = array()): string {
     return $templates->render("admin::create_event", array(
         "title_err" => $title_err,
         "description_err" => $description_err,
@@ -21,13 +22,13 @@ function render_page($templates, $title_err = false, $description_err = false, $
         "reg_date_err" => $reg_date_err,
         "event_date_err" => $event_date_err,
         "event_reg_date_err" => $event_reg_date_err,
-        "title_value" => isset($data["title_value"]) ? $data["title_value"]:"",
-        "description" => isset($data["description"]) ? $data["description"]:"",
-        "email_template" => isset($data["email_template"]) ? $data["email_template"]:"",
-        "reg_startdate_val" => isset($data["reg_startdate_val"]) ? $data["reg_startdate_val"]:"",
-        "reg_enddate_val" => isset($data["reg_enddate_val"]) ? $data["reg_enddate_val"]:"",
-        "event_startdate_val" => isset($data["event_startdate_val"]) ? $data["event_startdate_val"]:"",
-        "event_enddate_val" => isset($data["event_enddate_val"]) ? $data["event_enddate_val"]:"",
+        "title_value" => $data["title_value"] ?? "",
+        "description" => $data["description"] ?? "",
+        "email_template" => $data["email_template"] ?? "",
+        "reg_startdate_val" => $data["reg_startdate_val"] ?? date("Y-m-d H:i"),
+        "reg_enddate_val" => $data["reg_enddate_val"] ?? date("Y-m-d H:i"),
+        "event_startdate_val" => $data["event_startdate_val"] ?? date("Y-m-d H:i"),
+        "event_enddate_val" => $data["event_enddate_val"] ?? date("Y-m-d H:i"),
     ));
 }
 
