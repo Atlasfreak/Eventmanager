fe3fac25f4eb0ff00ae602a6502abffbe4939174
Per <atlasfreak@gmx.de>
2021-12-15 19:33:22 +0100

    fixed login;

--- admin/login.php
+++ admin/login.php
@@ -1,7 +1,7 @@
 <?php
 function verify_password(string $username, string $password, Database $db) {
     $sql = "SELECT pass, username FROM admin WHERE username=?";
-    $qry = $db->query($sql);
+    $qry = $db->query($sql, array($username));
     if($data = $qry->fetch()) {
         $data_password = $data["pass"];
         $data_username = $data["username"];
