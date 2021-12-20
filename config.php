f5c8a67ddab6a58978fdd0118919d0db8507229a
Per <atlasfreak@gmx.de>
2021-12-20 10:29:15 +0100

    added config file;

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
-define('ANMELDUNG_URL', '/anmeldung'); // relativer Pfad zur Domain
+define("ANMELDUNG_URL", "/anmeldung"); // relativer Pfad zur Domain
 
+function get_config() {
+    $filename = "config.ini";
+    $ini_array = parse_ini_file($filename, true);
+    return $ini_array;
+}
+
+define("CONFIG_DATA", get_config());
 ?>
