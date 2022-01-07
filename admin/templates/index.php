ea9213f765bac6a0e9b9d5115d35132ee98517dc
Per <atlasfreak@gmx.de>
2022-01-07 02:35:04 +0100

    added div for messages;

--- admin/templates/index.php
+++ admin/templates/index.php
@@ -1,10 +1,12 @@
 <?=$this->layout("admin::layout", ["title" => "Admin-".$title, "no_logout" => isset($no_logout) ? $no_logout:false])?>
 
-<?php if(isset($messages) and $messages): ?>
-    <?php foreach($messages as $message): ?>
-        <?=$this->insert("main::alert", ["type" => $message["type"], "msg" => $message["msg"]])?>
-    <?php endforeach ?>
-<?php endif ?>
+<div id="messages">
+    <?php if(isset($messages) and $messages): ?>
+        <?php foreach($messages as $message): ?>
+            <?=$this->insert("main::alert", ["type" => $message["type"], "msg" => $message["msg"]])?>
+        <?php endforeach ?>
+    <?php endif ?>
+</div>
 
 <h1 class="pt-3"><?=$this->e($title)?></h1>
 <?=$this->section("description")?>
