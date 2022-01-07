c077aaf3a448e08724db686b829d83ed20a93846
Per <atlasfreak@gmx.de>
2022-01-07 02:34:46 +0100

    added delete button functionality; added warning modal before deleting event;

--- admin/templates/home.php
+++ admin/templates/home.php
@@ -37,7 +37,14 @@
                             <a href="results.php?event=<?=$this->e($event["id"])?>"><?=$this->e($event["titel"])?></a>
                         </td>
                         <td>
-                            <button class="btn btn-danger btn-sm"><i class="bi bi-trash-fill"></i> Löschen</button>
+                            <button
+                            class="btn btn-danger btn-sm delete"
+                            data-event-id="<?=$this->e($event["id"])?>"
+                            data-event-title="<?=$this->e($event["titel"])?>"
+                            data-toggle="modal"
+                            data-target="#confirmDelete">
+                                <i class="bi bi-trash-fill"></i> Löschen
+                            </button>
                         </td>
                         <td>
                             <?php if($event["max_participants"] > 0):?>
@@ -64,6 +71,25 @@
             </tbody>
         </table>
     </div>
+    <div class="modal fade" id="confirmDeleteTemplate" tabindex="-1" data-link-id="#modalLink" aria-labelledby="confirmDeleteLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmDeleteLabel">{title} löschen?</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Die Veranstaltung, "{title}", wirklich endgültig löschen?
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-danger" id="modalLink"><i class="bi bi-trash-fill"></i> Endgültig löschen?</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="modalStore"></div>
 <?php else:?>
     <hr>
     <div class="alert alert-info" role="alert">
@@ -72,5 +98,27 @@
 <?php endif?>
 
 <div class="mt-3">
-    <a class="btn btn-success" role="button" href="create_event.php"><i class="bi bi-calendar-plus" style="font-size: 1.1rem;"></i> Veranstaltung hinzufügen</a>
+    <a class="btn btn-success" role="button" href="create_event.php"><i class="bi bi-calendar-plus" style="font-size: 1.1rem;">
+        </i> Veranstaltung hinzufügen
+    </a>
 </div>
+
+<?=$this->start("scripts")?>
+<script>
+    $("button.delete").click(function() {
+        let event_id = $(this).data("event-id");
+        let title = $(this).data("event-title");
+        let modal_store = $("div#modalStore");
+        modal_store.html($("#confirmDeleteTemplate").clone().attr("id", "confirmDelete"));
+        let modal = $("#confirmDelete");
+        let modal_btn_selector = "#confirmDelete .modal-footer > button";
+        $(modal_btn_selector).data("event-id", event_id);
+        modal.html(modal.html().replace(/{title}/gm, title));
+        $(modal_btn_selector).click(function() {
+            $.get("delete_event.php", {"event_id": event_id}).done(function(data) {
+                location.reload();
+            });
+        });
+    });
+</script>
+<?=$this->end()?>
