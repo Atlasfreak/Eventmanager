39bbb742a1be956e521608ae3aa74077079da35b
Per <atlasfreak@gmx.de>
2022-01-10 19:19:52 +0100

    added function to get raw config file contents; added function to replace config file; added global variable for config file;

--- config.php
+++ config.php
@@ -1,10 +1,21 @@
 <?php
 define("ANMELDUNG_URL", "/anmeldung"); // relativer Pfad zur Domain
+define("CONFIG_FILENAME", "config.ini");
 
 function get_config() {
-    $filename = "config.ini";
+    $filename = CONFIG_FILENAME;
     $ini_array = parse_ini_file($filename, true);
     return $ini_array;
+}
+
+function get_raw_config() {
+    $filename = CONFIG_FILENAME;
+    return file_get_contents($filename, true);
+}
+
+function overwrite_config($data) {
+    $filename = CONFIG_FILENAME;
+    return file_put_contents($filename, $data, FILE_USE_INCLUDE_PATH | LOCK_EX);
 }
 
 define("CONFIG_DATA", get_config());
