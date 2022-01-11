e2b02c86ac5b8d0c2001e73949c3673dfc28caf4
Per <atlasfreak@gmx.de>
2022-01-11 23:12:33 +0100

    get_event_data now only returns error type; render_event_error renders the error given by get_event_data; fixed spelling;

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 include("inc/db.php");
 include("inc/header.php");
 
-function render_regsitration(\League\Plates\Engine $templates, array $data): string {
+function render_registration(\League\Plates\Engine $templates, array $data): string {
     return $templates->render("main::event_registration", array(
         "title" => $data["title"],
-        "description" => $data["description"], // ACHTUNG dieser werd wird nicht escaped. Hier wird HTML erwartet, in der Datenbank ist KEIN HTML hinterlegt!
+        "description" => $data["description"], // ACHTUNG dieser Wert wird nicht escaped. Hier wird HTML erwartet, in der Datenbank ist KEIN HTML hinterlegt!
         "days" => $data["days"],
         "event_id" => $data["event_id"],
         "errors" => $data["errors"] ?? array(),
@@ -30,7 +30,7 @@ function render_overview(\League\Plates\Engine $templates, array $data): string 
     ));
 }
 
-function render_events_data(\League\Plates\Engine $templates, Database $db, array $data) {
+function render_events_data(\League\Plates\Engine $templates, Database $db, array $data = array()) {
     $data = array_merge($data, get_events_data($db));
     echo render_overview($templates, $data);
     exit;
@@ -47,17 +47,17 @@ function get_events_data(Database $db): array {
     );
 }
 
-function get_event_data(int $id, Database $db, \League\Plates\Engine $templates): array {
-    $sql_event = "SELECT beschreibung, titel, anmeldestart, anmeldeende FROM veranstaltungen WHERE id = ? AND anmeldestart <= CURRENT_TIMESTAMP AND anmeldeende >= CURRENT_TIMESTAMP";
+function get_event_data(int $id, Database $db): array {
+    $sql_event = "SELECT beschreibung, titel, anmeldestart, anmeldeende, stationen FROM veranstaltungen WHERE id = ? AND anmeldestart <= CURRENT_TIMESTAMP AND anmeldeende >= CURRENT_TIMESTAMP";
     $query_event = $db->query($sql_event, array($id));
     if ($query_event->rowCount() === 0){
-        render_events_data($templates, $db, ["errors" => [["msg" => "Für diese Veranstaltung kann man sich nicht anmelden."]]]);
+        return array("error" => "closed");
     }
 
     $sql_days = "SELECT tagDatum, tagID FROM tage WHERE veranstaltungsId = ? ORDER BY tagDatum";
     $query_days = $db->query($sql_days, array($id));
     if ($query_days->rowCount() === 0) {
-        render_events_data($templates, $db, ["errors" => [["msg" => "Dieser Veranstaltung wurden keine Tage zugewiesen, bitte schauen sie später nochmal vorbei."]]]);
+        return array("error" => "no_days");
     }
 
     $data_event = $query_event->fetch();
@@ -70,6 +70,18 @@ function get_event_data(int $id, Database $db, \League\Plates\Engine $templates)
         "days" => $data_days,
         "event_id" => $_GET["event"],
     );
+}
+
+function render_event_errors(\League\Plates\Engine $templates, Database $db, array $data) {
+    if (!isset($data["error"])) return;
+
+    if ($data["error"] === "closed") {
+        $msg = "Für diese Veranstaltung kann man sich nicht anmelden.";
+    } elseif ($data["error"] === "no_days") {
+        $msg = "Dieser Veranstaltung wurden keine Tage zugewiesen. Bitte verusche es später nochmal.";
+    }
+
+    render_events_data($templates, $db, ["errors" => [["msg" => $msg]]]);
 }
 
 function check_val(array $data, string $key) {
@@ -86,14 +98,13 @@ if (isset($_GET["event"])) {
         include("send.php");
         exit;
     }
-    $data = get_event_data($_GET["event"], $db, $templates);
+    $data = get_event_data($_GET["event"], $db);
+    render_event_errors($templates, $db, $data);
 
-    echo render_regsitration($templates, $data);
+    echo render_registration($templates, $data);
     exit;
 }
 
-$data = get_events_data($db);
-
-echo render_overview($templates, $data);
+echo render_events_data($templates, $db);
 
 ?>
