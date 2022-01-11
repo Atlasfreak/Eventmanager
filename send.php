a2526b4e1dd0e613f43c3bed25c80a533d39d2ce
Per <atlasfreak@gmx.de>
2022-01-11 23:14:22 +0100

    prefixed data vars with template; replaced event query to get all event data once; fixed spelling;

--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_GET["event"], $_POST)) {
-    $data_events = array("errors" => []);
+    $template_data_events = array("errors" => []);
 
     $max_participants = $db->get_max_participants($_GET["event"]);
     $current_participants = $db->get_participants($_GET["event"]);
@@ -9,7 +9,7 @@ if (isset($_GET["event"], $_POST)) {
     $registered_participants = 1; // TODO Möglichkeit für mehrere Anmeldungen implementieren.
 
     if ($current_participants >= $max_participants) {
-        array_push($data_events["errors"], array("msg" => "Es gibt für diese Veranstaltung keine freien Anmeldeplätze mehr."));
+        array_push($template_data_events["errors"], array("msg" => "Es gibt für diese Veranstaltung keine freien Anmeldeplätze mehr."));
     }
 
     $db_data = array(
@@ -39,35 +39,34 @@ if (isset($_GET["event"], $_POST)) {
     $query_already_registered = $db->query($sql_already_registered, $already_registered_data);
 
     if ($query_already_registered->rowCount() > 0) {
-        array_push($data_events["errors"], array("msg" => "Sie haben sich bereits für diese Veranstaltung registriert."));
+        array_push($template_data_events["errors"], array("msg" => "Sie haben sich bereits für diese Veranstaltung registriert."));
     }
 
-    $sql_event = "SELECT id FROM veranstaltungen WHERE id = ? AND anmeldestart <= CURRENT_TIMESTAMP AND anmeldeende >= CURRENT_TIMESTAMP";
-    $query_event = $db->query($sql_event, array($_GET["event"]));
-    if ($query_event->rowCount() === 0){
-        $data_events["errors"] = [];
-        array_push($data_events["errors"], array("msg" => "Das Anmeldefenster für diese Veranstaltung ist geschlossen."));
+    $data_event = get_event_data($_GET["event"], $db);
+    if (isset($data_event["error"]) and $data_event["error"] === "closed"){
+        $template_data_events["errors"] = [];
+        array_push($template_data_events["errors"], array("msg" => "Das Anmeldefenster für diese Veranstaltung ist geschlossen."));
     }
 
-    if (!($data_events["errors"] === [])) {
-        render_events_data($templates, $db, $data_events);
+    if (!($template_data_events["errors"] === [])) {
+        render_events_data($templates, $db, $template_data_events);
     }
 
     $captcha = htmlspecialchars($_POST["captcha"]);
 
-    $data_event = array("errors" => []);
+    $template_data_event = array("errors" => []);
 
     session_start();
 
     if (!(isset($_POST["captcha"]) and $captcha == $_SESSION['digit'])) {
-        $data_event["errors"]["captcha"] = "wrong";
+        $template_data_event["errors"]["captcha"] = "wrong";
     }
 
     $post_key = ["selected_day", "selected_timewindow", "lastname", "firstname", "email", "street", "house_nr", "postal_code", "city", "phone"];
 
     foreach ($post_key as $key) {
         if (!(isset($_POST[$key])) or $_POST[$key] === "") {
-            $data_event["errors"][$key] = "empty";
+            $template_data_event["errors"][$key] = "empty";
         }
     }
 
@@ -75,32 +74,32 @@ if (isset($_GET["event"], $_POST)) {
         $sql_timewindow_count = "SELECT COUNT(zeitfensterID) FROM zeitfenster WHERE tagID = ? AND zeitfensterID = ?";
         $query_timewindow_count = $db->query($sql_timewindow_count, array($_POST["selected_day"], $_POST["selected_timewindow"]));
         if($query_timewindow_count->rowCount() === 0 or ((int) $query_timewindow_count->fetch()[0]) !== 1) {
-            $data_event["errors"]["selected_day"] = "wrong_window";
-            $data_event["errors"]["selected_timewindow"] = "wrong_window";
+            $template_data_event["errors"]["selected_day"] = "wrong_window";
+            $template_data_event["errors"]["selected_timewindow"] = "wrong_window";
         } else {
             $timewindow_max_participants = $db->get_max_participants(null, array($_POST["selected_timewindow"]));
             $timewindow_participants = $db->get_participants(null, array($_POST["selected_timewindow"]));
             if ($timewindow_participants >= $timewindow_max_participants) {
-                $data_event["errors"]["selected_timewindow"] = "already_full";
+                $template_data_event["errors"]["selected_timewindow"] = "already_full";
             } elseif ($timewindow_participants + $registered_participants > $max_participants) {
-                $data_event["errors"]["selected_timewindow"] = "too_many_registered";
+                $template_data_event["errors"]["selected_timewindow"] = "too_many_registered";
             }
         }
     }
     if (isset($_POST["email"])) {
         if (!(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))) {
-            $data_event["errors"]["email"] = "invalid";
+            $template_data_event["errors"]["email"] = "invalid";
         }
     }
 
-    if (!($data_event["errors"] === [])) {
+    if (!($template_data_event["errors"] === [])) {
         foreach ($post_key as $key) {
             if (isset($_POST[$key])) {
-                $data_event["values"][$key] = htmlspecialchars($_POST[$key]);
+                $template_data_event["values"][$key] = htmlspecialchars($_POST[$key]);
             }
         }
-        $data_event = array_merge($data_event, get_event_data($_GET["event"], $db, $templates));
-        echo render_regsitration($templates, $data_event);
+        $template_data_event = array_merge($template_data_event, $data_event);
+        echo render_registration($templates, $template_data_event);
         exit;
     }
 
@@ -110,22 +109,22 @@ if (isset($_GET["event"], $_POST)) {
     try {
         $db->insert("teilnehmer", $db_data);
     } catch (Exception $exception) {
-        array_push($data_events["errors"], ["msg" => "Es gab ein Problem mit ihren Angaben. Bitte melden sie sich bei team@whgonline.de falls dieses Problem weiterhin besteht."]);
-        render_events_data($templates, $db, $data_events);
+        array_push($template_data_events["errors"], ["msg" => "Es gab ein Problem mit ihren Angaben. Bitte melden sie sich bei team@whgonline.de falls dieses Problem weiterhin besteht."]);
+        render_events_data($templates, $db, $template_data_events);
     }
 
     // Send confirmation E-Mail
     $participant_id = $db->mysql->lastInsertID();
     include("inc/mail.php");
     if (!send_confirmation_mail($db, $participant_id)) {
-        array_push($data_events["errors"], ["msg" => "Es gab ein Problem beim versenden der Bestätigungs E-Mail, ihre Daten wurden bereits gespeichert! Bitte melden sie sich bei anmeldung@whgonline.de"]);
-        render_events_data($templates, $db, $data_events);
+        array_push($template_data_events["errors"], ["msg" => "Es gab ein Problem beim versenden der Bestätigungs E-Mail, ihre Daten wurden bereits gespeichert! Bitte melden sie sich bei anmeldung@whgonline.de"]);
+        render_events_data($templates, $db, $template_data_events);
     }
-    $data_events["messages"] = [[
+    $template_data_events["messages"] = [[
         "type" => "success",
         "msg" => "Die Anmeldung war erfolgreich. Sie sollten in kürze eine E-Mail erhalten. Schauen sie ggf. im Spamordner nach.",
         ]];
-    render_events_data($templates, $db, $data_events);
+    render_events_data($templates, $db, $template_data_events);
 }
 
 ?>
