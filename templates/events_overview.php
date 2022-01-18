e482934eadf7a754d38814d2d9bb490f04dbfc81
Per <atlasfreak@gmx.de>
2022-01-18 02:06:28 +0100

    fixed events_overview table not being responsive;

--- templates/events_overview.php
+++ templates/events_overview.php
@@ -18,34 +18,36 @@
         <?=$this->insert("main::alert", ["type" => $message["type"], "msg" => $message["msg"]])?>
     <?php endforeach ?>
 <?php endif ?>
-<table class="table">
-    <?=$this->insert("main::table_head", ["ueberschriften" => ["Veranstaltung", "Beschreibung", "Anmeldeschluss", "Links"]])?>
-    <tbody>
-        <?php foreach($events as $event): ?>
-            <tr>
-                <td><?=$this->e($event["titel"])?></td>
-                <td>
-                    <a
-                    class="text-toggle"
-                    href="#description_<?=$this->e($event["id"])?>"
-                    data-toggle="collapse"
-                    aria-expanded="false"
-                    aria-controls="description_<?=$this->e($event["id"])?>"
-                    >
-                        <span class="text-collapsed">ausklappen <i class="bi bi-chevron-down"></i></span>
-                        <span class="text-expanded">einklappen <i class="bi bi-chevron-up"></i></span>
-                    </a>
-                    <div class="collapse" id="description_<?=$this->e($event["id"])?>">
-                        <?=parse_delta($event["beschreibung"])?>
-                    </div>
-                </td>
-                <td>
-                    <?=$this->e(strftime("%A %d.%m.%Y %H:%M", strtotime($event["anmeldeende"])))?>
-                </td>
-                <td>
-                    <a href="?event=<?=$this->e($event["id"])?>">Anmelden <i class="bi bi-arrow-right"></i></a>
-                </td>
-            </tr>
-        <?php endforeach ?>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table">
+        <?=$this->insert("main::table_head", ["ueberschriften" => ["Veranstaltung", "Beschreibung", "Anmeldeschluss", "Links"]])?>
+        <tbody>
+            <?php foreach($events as $event): ?>
+                <tr>
+                    <td><?=$this->e($event["titel"])?></td>
+                    <td>
+                        <a
+                        class="text-toggle"
+                        href="#description_<?=$this->e($event["id"])?>"
+                        data-toggle="collapse"
+                        aria-expanded="false"
+                        aria-controls="description_<?=$this->e($event["id"])?>"
+                        >
+                            <span class="text-collapsed">ausklappen <i class="bi bi-chevron-down"></i></span>
+                            <span class="text-expanded">einklappen <i class="bi bi-chevron-up"></i></span>
+                        </a>
+                        <div class="collapse" id="description_<?=$this->e($event["id"])?>">
+                            <?=parse_delta($event["beschreibung"])?>
+                        </div>
+                    </td>
+                    <td>
+                        <?=$this->e(strftime("%A %d.%m.%Y %H:%M", strtotime($event["anmeldeende"])))?>
+                    </td>
+                    <td>
+                        <a href="?event=<?=$this->e($event["id"])?>">Anmelden <i class="bi bi-arrow-right"></i></a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</div>
