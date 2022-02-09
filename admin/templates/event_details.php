94b004b29224bc4b8a9f2ad7c4266eeb04508ca2
Per Göttlicher <atlasfreak@gmx.de>
2022-02-09 13:08:41 +0100

    added icons; added new errors; removed unused variables;

--- admin/templates/event_details.php
+++ admin/templates/event_details.php
@@ -1,41 +1,41 @@
-<?=$this->layout("admin::layout", ["title" => "Test Veranstaltung"])?>
+<?=$this->layout("admin::layout", ["title" => $title_value])?>
 
 <?=$this->start("styles")?>
     <?=$this->insert("admin_inc::event_form_css")?>
 <?=$this->end()?>
 
-<h1 class="mb-4">Test Veranstaltung Übersicht</h1>
+<h1 class="mb-4"><?=$this->e($title_value)?> Übersicht</h1>
 <div class="row">
     <div class="col-md-3">
         <div class="nav flex-column nav-pills" id="id" role="tablist" aria-orientation="vertical">
-            <a class="nav-link active" id="edit-tab" data-toggle="pill" href="#edit" role="tab" aria-controls="edit" aria-selected="true">Veranstaltung bearbeiten</a>
-            <a class="nav-link" id="e-mails-tab" data-toggle="pill" href="#e-mails" role="tab" aria-controls="e-mails" aria-selected="false">E-Mails</a>
+            <a class="nav-link active" id="edit-tab" data-toggle="pill" href="#edit" role="tab" aria-controls="edit" aria-selected="true"><i class="bi bi-pencil-square"></i> Veranstaltung bearbeiten</a>
+            <a class="nav-link" id="e-mails-tab" data-toggle="pill" href="#e-mails" role="tab" aria-controls="e-mails" aria-selected="false"><i class="bi bi-envelope-fill"></i> E-Mails</a>
         </div>
         <div class="nav flex-column nav-pills">
-            <a class="nav-link" href="results.php?event=<?=$this->e($id)?>">Teilnehmerübersicht</a>
+            <a class="nav-link" href="results.php?event=<?=$this->e($id)?>"><i class="bi bi-table"></i> Teilnehmerübersicht</a>
         </div>
     </div>
     <div class="col-md-9">
         <div class="tab-content" id="tabContent">
             <div class="tab-pane fade show active" id="edit" role="tabpanel" aria-labelledby="edit-tab">
-                <form id="edit_event_form" action="edit.php" method="post">
+                <form id="event_form" action="event_details.php?event_id=<?=$this->e($id)?>" method="post">
                     <?=$this->insert("admin_inc::event_form", [
-                        "title_err" => $title_err ?? false,
-                        "description_err" => $description_err ?? false,
-                        "email_template_err" => $email_template_err ?? false,
-                        "reg_date_err" => $reg_date_err ?? false,
-                        "event_date_err" => $event_date_err ?? false,
-                        "event_reg_date_err" => $event_reg_date_err ?? false,
+                        "title_err" => $errors["title"] ?? false,
+                        "description_err" => $errors["description"] ?? false,
+                        "email_template_err" => $errors["email_template"] ?? false,
+                        "reg_date_err" => $errors["reg_date"] ?? false,
                         "title_value" => $title_value,
                         "description" => $description,
                         "email_template" => $email_template,
                         "reg_startdate_val" => $reg_startdate_val,
                         "reg_enddate_val" => $reg_enddate_val,
                         ])?>
-                    <button type="submit" class="btn btn-success"><i class="bi bi-pencil-square"></i> Veranstaltung bearbeiten</button>
+                    <button type="submit" class="btn btn-success"><i class="bi bi-pencil-square"></i> Veranstaltung speichern</button>
                 </form>
             </div>
-            <div class="tab-pane fade" id="e-mails" role="tabpanel" aria-labelledby="e-mails-tab">E-Mails</div>
+            <div class="tab-pane fade" id="e-mails" role="tabpanel" aria-labelledby="e-mails-tab">
+                <?=$this->insert("admin_inc::mails")?>
+            </div>
         </div>
     </div>
 </div>
