e3eeda52aee442788b562c12a1e08c9d773a88d7
Per <atlasfreak@gmx.de>
2022-02-12 19:25:16 +0100

    added dynamic required attribute; fixed mail form submit;

--- admin/templates/include/mails_scripts.php
+++ admin/templates/include/mails_scripts.php
@@ -8,14 +8,18 @@
     });
 
     let mail_editors = [[create_editor("email_editor"), "email_editor"]];
-    $("mail_form").submit(function(e) {
+    $("#mail_form").submit(function(e) {
         mail_editors.forEach(element => {submit_editor(element[0], element[1], e)});
     });
     $("#auto_email").change(function(e) {
         if (this.checked) {
             $("#email_msg").hide();
+            $("#email_subject").attr("required", false);
+            $("#email").attr("required", false);
         } else {
             $("#email_msg").show();
+            $("#email_subject").attr("required", true);
+            $("#email").attr("required", true);
         }
     });
 </script>
