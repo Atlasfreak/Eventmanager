58dd8ec5d85f55c75c72a865dc28a678b0f54039
Per <atlasfreak@gmx.de>
2022-02-16 01:00:09 +0100

    base is inlcuded before sessions start;

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,6 +1,6 @@
 <?php
-    if(session_status() !== PHP_SESSION_ACTIVE and empty($_SESSION)) session_start();
     include(__DIR__."/../../inc/header_base.php");
+    if(session_status() !== PHP_SESSION_ACTIVE and empty($_SESSION)) session_start();
 
     $templates->addFolder("admin_inc", __DIR__."/../templates/include");
 
