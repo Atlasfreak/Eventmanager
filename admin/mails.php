502f4a424a1ebdff2ac670b0e735ef6c5d759878
Per <atlasfreak@gmx.de>
2022-02-16 19:16:09 +0100

    added input validation;

--- admin/mails.php
+++ admin/mails.php
@@ -5,33 +5,51 @@ include(__DIR__."/../inc/mail.php");
 
 if (empty($_POST)) exit_with_code(400);
 
-$in  = str_repeat('?,', count($_POST["email_addresses"]) - 1) . '?';
-$email_sql = "SELECT email,
-        id,
-        vorname,
-        nachname
-    FROM teilnehmer
-    WHERE id IN (".$in.")";
+$email_addresses = $_POST["email_addresses"] ?? null;
+$subject = $_POST["email_subject"] ?? null;
+$content = $_POST["email"] ?? null;
+$auto_email = $_POST["auto_email"] ?? null;
 
-$email_query = $db->query($email_sql, $_POST["email_addresses"]);
-$email_data = $email_query->fetchAll();
+$data = array();
 
-$subject = $_POST["email_subject"];
-$content = $_POST["email"];
-
-if (!empty($_POST["auto_email"])) {
-    function email($email_address, $id, $name, $subject, $content, $db) {
-        send_confirmation_mail($db, $id);
+if (empty($email_addresses)) {
+    $data["errors"]["email_addresses"] = "empty";
+}
+if (empty($auto_email)) {
+    if (empty($subject)) {
+        $data["errors"]["email_subject"] = "empty";
     }
-} else {
-    function email($email_address, $id, $name, $subject, $content, $db) {
-        send_mail($email_address, $name, $subject, parse_delta($content));
+    if (empty($content)) {
+        $data["errors"]["email"] = "empty";
     }
 }
 
-foreach ($email_data as $email_address) {
-    email($email_address["email"], $email_address["id"], $email_address["nachname"]." ".$email_address["vorname"], $subject, $content, $db);
+if (empty($data["errors"])) {
+    $in  = str_repeat('?,', count($email_addresses) - 1) . '?';
+    $email_sql = "SELECT email,
+            id,
+            vorname,
+            nachname
+        FROM teilnehmer
+        WHERE id IN (".$in.")";
+
+    $email_query = $db->query($email_sql, $email_addresses);
+    $email_data = $email_query->fetchAll();
+
+    if (!empty($auto_email)) {
+        function email($email_address, $id, $name, $subject, $content, $db) {
+            send_confirmation_mail($db, $id);
+        }
+    } else {
+        function email($email_address, $id, $name, $subject, $content, $db) {
+            send_mail($email_address, $name, $subject, parse_delta($content));
+        }
+    }
+
+    foreach ($email_data as $email_address) {
+        email($email_address["email"], $email_address["id"], $email_address["nachname"]." ".$email_address["vorname"], $subject, $content, $db);
+    }
+    redirect($_SERVER['REQUEST_URI']);
 }
-redirect($_SERVER['REQUEST_URI']);
 
 ?>
