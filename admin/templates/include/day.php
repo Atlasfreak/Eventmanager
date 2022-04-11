3fc29685f4b9ea747bffab30fd8fdcb14c6a7b50
Per <atlasfreak@gmx.de>
2022-04-11 04:33:49 +0200

    added template for day input

---
+++ admin/templates/include/day.php
@@ -0,0 +1,10 @@
+<div class="form-row">
+    <input type="hidden" name="<?=$this->e($extra_field ?? null)?>" value="true">
+    <input type="hidden" name="day_id" value="<?=$this->e($id)?>">
+    <label for="day_date_<?=$this->e($id)?>" class="col-auto col-form-label">Datum</label>
+    <div class="col-auto">
+        <input type="date" class="form-control" id="day_date_<?=$this->e($id)?>" name="day_date_<?=$this->e($id)?>"
+            value="<?=$this->e($date ?? null)?>">
+        <div class="invalid-feedback"></div>
+    </div>
+</div>
