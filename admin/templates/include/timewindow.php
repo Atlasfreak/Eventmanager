2bcb2b7455f17882baabc255236f3f157795abf7
Per <atlasfreak@gmx.de>
2022-04-11 04:34:03 +0200

    added template for timewindow input

---
+++ admin/templates/include/timewindow.php
@@ -0,0 +1,29 @@
+<div class="row">
+    <input type="hidden" name="timewindow_id" value="<?=$this->e($id)?>">
+    <input type="hidden" name="<?=$this->e($extra_field ?? null)?>" value="true">
+    <label for="timewindow_from_<?=$this->e($id)?>_<?=$this->e($day_id)?>" class="col-xl-auto col-form-label pr-xl-0">
+        Von
+    </label>
+    <div class="col-xl-3 w-xl-20">
+        <input type="time" class="form-control" id="timewindow_from_<?=$this->e($id)?>_<?=$this->e($day_id)?>"
+            name="timewindow_from_<?=$this->e($id)?>" value="<?=$this->e($from ?? null)?>">
+        <div class="invalid-feedback"></div>
+    </div>
+    <label for="timewindow_until_<?=$this->e($id)?>_<?=$this->e($day_id)?>" class="col-xl-auto col-form-label px-xl-0">
+        Bis (optional)
+    </label>
+    <div class="col-xl-3 w-xl-20">
+        <input type="time" class="form-control" id="timewindow_until_<?=$this->e($id)?>_<?=$this->e($day_id)?>"
+            name="timewindow_until_<?=$this->e($id)?>" value="<?=$this->e($until ?? null)?>">
+        <div class="invalid-feedback"></div>
+    </div>
+    <label for="timewindow_max_participants_<?=$this->e($id)?>_<?=$this->e($day_id)?>" class="col-xl-auto col-form-label px-xl-0">
+        Max. Teilnehmer
+    </label>
+    <div class="col-xl-2">
+        <input type="number" name="timewindow_max_participants_<?=$this->e($id)?>"
+            id="timewindow_max_participants_<?=$this->e($id)?>_<?=$this->e($day_id)?>" class="form-control" min="1"
+            value="<?=$this->e($max_participants ?? null)?>">
+        <div class="invalid-feedback"></div>
+    </div>
+</div>
